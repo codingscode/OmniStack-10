b962b8bbbe05d89f320acd5932a1cda45538f590
Heronnes Pereira <heronnestg@hotmail.com>
2020-11-27 12:10:23 -0300

    parte 7

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,15 @@ const express = require('express')
 
 const app = express()
 
+// métodos http: get, post, put, delete
+
+/* Tipos de parametros:
+
+Query Params:
+Route Params:
+Body:
+ */
+
 app.get('/', (requisicao, resposta) => {
     return resposta.json({menssagem: 'Olá OminiStack'})
 })
