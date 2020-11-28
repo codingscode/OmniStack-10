c266639eb729c74afa219ebe98cbb3781cbde2d5
Heronnes Pereira <heronnestg@hotmail.com>
2020-11-28 12:12:14 -0300

    parte 13

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-kdxee.mongodb.net/week10?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+
+})
+// mongodb+srv://omnistack:omnistack@cluster0-kdxee.mongodb.net/week10?retryWrites=true&w=majority
+// mongodb+srv://omnistack:<password>@cluster0-kdxee.mongodb.net/test?retryWrites=true&w=majority
+// mongodb+srv://omnistack:omnistack@cluster0-kdxee.mongodb.net/week10?retryWrites=true&w=majority
+
+
 app.use(express.json()) // válido para todos
 
 // métodos http: get, post, put, delete
@@ -11,6 +22,9 @@ app.use(express.json()) // válido para todos
 Query Params:  req.query (Filtros, ordenação, paginação, ...)
 Route Params:  request.params (identificar um recurso na alteração ou remoção)
 Body: request.body (Dados para criação ou alteração de um registro)
+
+
+
  */
 
 app.post('/usuarios', (requisicao, resposta) => {
